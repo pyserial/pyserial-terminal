9b3c6da556c9a0bc88250f483007855f24e709d6
Chris Liechti <cliechti@gmx.net>
2018-12-14 01:42:26 +0100

    feat: map 0x00...0x1f to key names

--- serial_terminal/console/tk_widget.py
+++ serial_terminal/console/tk_widget.py
@@ -74,6 +74,42 @@ keymap = {
 }
 
 
+MAP_CONTROL_KEYS = {
+    '\x00': 'Ctrl+Space',
+    '\x01': 'Ctrl+A',
+    '\x02': 'Ctrl+B',
+    '\x03': 'Ctrl+C',
+    '\x04': 'Ctrl+D',
+    '\x05': 'Ctrl+E',
+    '\x06': 'Ctrl+F',
+    '\x07': 'Ctrl+G',
+    '\x08': 'Ctrl+H',
+    '\x09': 'Tab',  # 'Ctrl+I',
+    '\x0a': 'Ctrl+J',
+    '\x0b': 'Ctrl+K',
+    '\x0c': 'Ctrl+L',
+    '\x0d': 'Ctrl+M',
+    '\x0e': 'Ctrl+N',
+    '\x0f': 'Ctrl+O',
+    '\x10': 'Ctrl+P',
+    '\x11': 'Ctrl+Q',
+    '\x12': 'Ctrl+R',
+    '\x13': 'Ctrl+S',
+    '\x14': 'Ctrl+T',
+    '\x15': 'Ctrl+U',
+    '\x16': 'Ctrl+V',
+    '\x17': 'Ctrl+W',
+    '\x18': 'Ctrl+X',
+    '\x19': 'Ctrl+Y',
+    '\x1a': 'Ctrl+Z',
+    '\x1b': 'Esc',  # 'Ctrl+[',
+    '\x1c': 'Ctrl+\\',
+    '\x1d': 'Ctrl+]',
+    '\x1e': 'Ctrl+^',
+    '\x1f': 'Ctrl+_',
+}
+
+
 class Console(scrolledtext.ScrolledText):
     def __init__(self, *args, **kwargs):
         # super().__init__(width=80, height=25)
@@ -134,7 +170,10 @@ class Console(scrolledtext.ScrolledText):
         pass
 
     def _send_key(self, key):
-        self._input_queue.put(key)
+        if key in MAP_CONTROL_KEYS:
+            self._input_queue.put(MAP_CONTROL_KEYS[key])
+        else:
+            self._input_queue.put(key)
         return "break"  # stop event propagation
 
     def getkey(self):
@@ -209,3 +248,23 @@ class Console(scrolledtext.ScrolledText):
             self.insert(f'{_y}.{x}', ' ' * width)
         #     if not selective:
         #         attrs
+
+
+if __name__ == "__main__":
+    import threading
+    # test code to show what key codes are generated
+    root = tk.Tk()
+    root.title('pySerial-Terminal tk_widget test')
+    console = Console(root)
+    console.pack()
+    console.focus_set()
+
+    def input_thread():
+        while True:
+            key = console.getkey()
+            console.write(f'{key!r} ')
+            if key == 'Ctrl+D':
+                root.quit()
+    t = threading.Thread(target=input_thread, daemon=True)
+    t.start()
+    root.mainloop()
--- serial_terminal/console/windows.py
+++ serial_terminal/console/windows.py
@@ -20,6 +20,41 @@ try:
 except NameError:
     pass
 
+MAP_CONTROL_KEYS = {
+    '\x00': 'Ctrl+Space',
+    '\x01': 'Ctrl+A',
+    '\x02': 'Ctrl+B',
+    '\x03': 'Ctrl+C',
+    '\x04': 'Ctrl+D',
+    '\x05': 'Ctrl+E',
+    '\x06': 'Ctrl+F',
+    '\x07': 'Ctrl+G',
+    '\x08': 'Ctrl+H',
+    '\x09': 'Tab',  # 'Ctrl+I',
+    '\x0a': 'Ctrl+J',
+    '\x0b': 'Ctrl+K',
+    '\x0c': 'Ctrl+L',
+    '\x0d': 'Ctrl+M',
+    '\x0e': 'Ctrl+N',
+    '\x0f': 'Ctrl+O',
+    '\x10': 'Ctrl+P',
+    '\x11': 'Ctrl+Q',
+    '\x12': 'Ctrl+R',
+    '\x13': 'Ctrl+S',
+    '\x14': 'Ctrl+T',
+    '\x15': 'Ctrl+U',
+    '\x16': 'Ctrl+V',
+    '\x17': 'Ctrl+W',
+    '\x18': 'Ctrl+X',
+    '\x19': 'Ctrl+Y',
+    '\x1a': 'Ctrl+Z',
+    '\x1b': 'Esc',  # 'Ctrl+[',
+    '\x1c': 'Ctrl+\\',
+    '\x1d': 'Ctrl+]',
+    '\x1e': 'Ctrl+^',
+    '\x1f': 'Ctrl+_',
+}
+
 MAP_F_KEYS = {
     '\x03': 'Ctrl+2',
     '\x3b': 'F1',
@@ -77,6 +112,7 @@ MAP_F_KEYS = {
     '\x75': 'Ctrl+KP_1',
     '\x76': 'Ctrl+KP_3',
     '\x77': 'Ctrl+KP_7',
+    '\x83': 'Ctrl+^',
     '\x84': 'Ctrl+KP_9',
     '\x8d': 'Ctrl+KP_8',
     '\x91': 'Ctrl+KP_2',
@@ -246,6 +282,8 @@ class Console(ConsoleBase):
                     return MAP_SPECIAL_KEYS[code]
                 except KeyError:
                     return 'unknown special key {!r} {:02x}'.format(code, ord(code))
+            elif z < '\x20':
+                return MAP_CONTROL_KEYS[z]
             else:
                 return z
 
@@ -331,5 +369,5 @@ if __name__ == "__main__":
     while True:
         key = console.getkey()
         print(repr(key))
-        if key == '\x04':  # exit with CTRL+D
+        if key == 'Ctrl+D':
             break
